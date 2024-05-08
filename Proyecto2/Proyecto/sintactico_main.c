6adb885f0bf7bd74bb5a79bd0c01137801fb467a
Eva Pina <116506327+evaapinaa@users.noreply.github.com>
2024-05-09 00:36:50 +0200

    Add files via upload

---
+++ Proyecto2/Proyecto/sintactico_main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern int numErroresSintacticos;
+extern int numErroresSemanticos;
+extern int numErroresLexicos;
+extern char *yytext;
+extern int  yyleng;
+extern FILE *yyin;
+extern int yyparse();
+FILE *fich;
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		printf("Uso correcto: %s fichero\n",argv[0]);
+		exit(1);
+	}
+	FILE *fich = fopen(argv[1],"r");
+	if (fich == 0) {
+		printf("No se puede abrir %s\n",argv[1]);
+		exit(1);
+	}
+    yyin=fich;
+    yyparse();
+	if (!(numErroresSintacticos == 0 && numErroresSemanticos == 0 && numErroresLexicos == 0)){
+		printf("--------------------\n");
+	 	printf(" Errores léxicos: %d\n", numErroresLexicos);
+		printf(" Errores sintácticos: %d\n", numErroresSintacticos);
+		printf(" Errores semánticos: %d\n", numErroresSemanticos);
+	}
+    fclose(fich);
+    return 0;
+}
+
+
